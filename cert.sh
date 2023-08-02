1b3dc83b03e1652b23cf18066b5afaa3b3364695
mecaota <24618982+mecaota@users.noreply.github.com>
2023-08-02 13:55:13 +0900

    証明書更新コマンドにも待機時間を30sにする設定を追加

--- cert.sh
+++ cert.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-readonly REQUIRE_ARGS="--dns-cloudflare --dns-cloudflare-credentials /certbot.ini -d msk.kessai-otaku.club -d media.kessai-otaku.club  --dns-cloudflare-propagation-seconds 30"
-
+readonly REQUIRE_ARGS="--dns-cloudflare --dns-cloudflare-credentials /certbot.ini -d msk.kessai-otaku.club -d media.kessai-otaku.club"
+readonly WAIT_ARGS="--dns-cloudflare-propagation-seconds 30"
 
 case $1 in
-  "new") docker-compose run certbot certonly $REQUIRE_ARGS ;;
-  "renew") docker-compose run certbot renew ;;
-  "renew-test") docker-compose run certbot renew --dry-run ;;
-  "test") echo $REQUIRE_ARGS ;;
+  "new") docker-compose run certbot certonly $REQUIRE_ARGS $WAIT_ARGS;;
+  "renew") docker-compose run certbot renew $WAIT_ARGS;;
+  "renew-test") docker-compose run certbot renew $WAIT_ARGS --dry-run ;;
+  "test") echo $REQUIRE_ARGS $WAIT_ARGS;;
   *) echo "Usage: cert.sh {new|renew|renew-test}" ;;
 esac
 
