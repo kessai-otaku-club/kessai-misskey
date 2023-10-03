d6c687ac4a6390454647f2d0fc2b71308e08a1f4
mecaota <24618982+mecaota@users.noreply.github.com>
2023-10-03 19:14:18 +0900

    certbotimageをタグバージョン指定化

--- cert.sh
+++ cert.sh
@@ -4,9 +4,9 @@ readonly REQUIRE_ARGS="--dns-cloudflare --dns-cloudflare-credentials /certbot.in
 readonly WAIT_ARGS="--dns-cloudflare-propagation-seconds 30"
 
 case $1 in
-  "new") docker-compose run certbot certonly $REQUIRE_ARGS $WAIT_ARGS;;
-  "renew") docker-compose run certbot renew $WAIT_ARGS;;
-  "renew-test") docker-compose run certbot renew $WAIT_ARGS --dry-run ;;
+  "new") docker compose run certbot certonly $REQUIRE_ARGS $WAIT_ARGS;;
+  "renew") docker compose run certbot renew $WAIT_ARGS;;
+  "renew-test") docker compose run certbot renew $WAIT_ARGS --dry-run ;;
   "test") echo $REQUIRE_ARGS $WAIT_ARGS;;
   *) echo "Usage: cert.sh {new|renew|renew-test}" ;;
 esac
